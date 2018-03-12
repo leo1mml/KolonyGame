98dfc101bfdecc3bb7f7ceb531f98e2d4be9f82f
Isaias Melo <ifm394@gmail.com>
2018-03-12 17:27:40 -0300

    creating EntityManagerBackgroundLayer

---
+++ KolonyGame Shared/GameScene/Layers/BackgroundLayer/Entities/EntityManagerBackgroundLayer.swift
@@ -0,0 +1,9 @@
+//
+//  EntityManagerBackgroundLayer.swift
+//  KolonyGame iOS
+//
+//  Created by Isaias Fernandes on 12/03/2018.
+//  Copyright © 2018 Leonel Menezes. All rights reserved.
+//
+
+import Foundation
