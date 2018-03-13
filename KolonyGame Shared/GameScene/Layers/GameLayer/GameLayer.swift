a2e1eb0376c1092a3c2f9be803e1e18224dfb872
leo1mml <mml_leo@hotmail.com>
2018-03-13 17:24:57 -0300

    added planet and black hole to game layer

--- KolonyGame Shared/GameScene/Layers/GameLayer/GameLayer.swift
+++ KolonyGame Shared/GameScene/Layers/GameLayer/GameLayer.swift
@@ -11,32 +11,46 @@ class GameLayer: SKNode {
     
     var size: CGSize?
     var entityManager : EntityManagerGameLayer?
+    var blackHole : BlackHoleEntity?
     
     init(size: CGSize) {
         super.init()
         self.size = size
         entityManager = EntityManagerGameLayer(gameLayer: self)
         
-        
     }
     
     
     func configureLayer() {
+        createBlackHole()
+    }
+    
+    
+    func createBlackHole() {
+        self.blackHole = BlackHoleEntity(imageName: "blackhole")
+        if let spriteComponent = blackHole?.component(ofType: SpriteComponent.self) {
+            spriteComponent.node.size.height = (self.size?.height)! * 0.31
+            spriteComponent.node.size.width = (self.size?.height)! * 0.31
+            spriteComponent.node.position = CGPoint(x: (self.size?.width)!/2, y: (self.size?.height)! * 0.73)
+        }
+        entityManager?.add(blackHole!)
         createPlanetOne()
     }
     
-//    let humanCastle = Castle(imageName: "castle1_atk", team: .team1)
-//    if let spriteComponent = humanCastle.component(ofType: SpriteComponent.self) {
-//        spriteComponent.node.position = CGPoint(x: spriteComponent.node.size.width/2, y: size.height/2)
-//    }
-//    entityManager.add(humanCastle)
-    
     func createPlanetOne() {
         let planetOne = PlanetEntity(imageName: "planet1")
-        if let spriteComponent = planetOne.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.position = CGPoint(x: (self.size?.width)!/2, y: (self.size?.height)!/2)
+        if let planetSpriteComponent = planetOne.component(ofType: SpriteComponent.self) {
+            planetSpriteComponent.node.size.height = (self.size?.height)! * 0.11
+            planetSpriteComponent.node.size.width = (self.size?.height)! * 0.11
+            if let blackHoleSprite = blackHole?.component(ofType: SpriteComponent.self) {
+                planetSpriteComponent.node.position.x = 0
+                planetSpriteComponent.node.position.y = -(blackHoleSprite.node.size.height/2)
+//                blackHoleSprite.node.addChild(planetSpriteComponent.node)
+                entityManager?.addPlanet(planetOne)
+            }
+            
         }
-        entityManager?.add(planetOne)
+        
     }
     
     
