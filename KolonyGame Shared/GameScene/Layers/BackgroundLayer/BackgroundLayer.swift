966b603b0e1f425f4150694f55d5824d15520f00
Isaias Melo <ifm394@gmail.com>
2018-04-05 00:57:48 -0300

    commenting some methods and removing white lines off code

--- KolonyGame Shared/GameScene/Layers/BackgroundLayer/BackgroundLayer.swift
+++ KolonyGame Shared/GameScene/Layers/BackgroundLayer/BackgroundLayer.swift
@@ -13,7 +13,9 @@ class BackgroundLayer: SKNode {
     
     let STARS_AMOUNT = 100
     
+    //layer size
     var size: CGSize?
+    
     var entityManager : EntityManagerBackgroundLayer?
     
     init(size: CGSize) {
@@ -24,13 +26,16 @@ class BackgroundLayer: SKNode {
 
     func setupLayer () {
         
+        //creating background
         var size = CGSize(width: (self.size?.width)!, height: (self.size?.height)!)
         let bg = BackgroundEntity(imageName: "bg", size: size)
         
+        //creating stars of background
         size = CGSize(width: (self.size?.height)! * 0.01, height: (self.size?.height)! * 0.01)
         let stars = createPoolStars(size, "stardefault")
         setup(stars)
         
+        //creating mist
         size = CGSize(width: (self.size?.height)! , height: (self.size?.height)!)
         let mist = StarEntity(imageName: "nevoas", size: size)
         
@@ -49,10 +54,10 @@ class BackgroundLayer: SKNode {
         }
     }
     
-    
     //Create ramdon position for stars and configure they
     func setup (_ stars: [StarEntity]) {
         for  i in stars {
+            
             var x = CGFloat(arc4random_uniform(UInt32(size!.width)))
             x -= (size?.width)! / 2
             var y = CGFloat(arc4random_uniform(UInt32(size!.height)))
@@ -68,16 +73,12 @@ class BackgroundLayer: SKNode {
                 //configuring scale effect
                 sprite.node.run(sprite.scaleAction(timeBetweenScale: 1, scaleMultiplier: NumbersUtil.randomCGFloat(min: 0.4, max: 1.5)))
                 
-                //configuring alpha fade effect                
+                //configuring alpha fade effect
                 sprite.node.run(sprite.alphaAction(alphaValue: NumbersUtil.randomCGFloat(min: 0, max: 0.8), duration: TimeInterval(NumbersUtil.randomCGFloat(min: 4, max: 6))))
 
             }
-            
-            
-            
         }
     }
-    
     
     //Create many stars for background
     func createPoolStars(_ size : CGSize, _ imageName: String) -> [StarEntity] {
@@ -88,6 +89,7 @@ class BackgroundLayer: SKNode {
         return stars
     }
     
+    //configure properties of a gkEntity
     func setupEntity<T: GKEntity>(entity: T, position: CGPoint, zPosition: CGFloat?) {
         if let spriteComponent = entity.component(ofType: SpriteComponent.self) {
             spriteComponent.node.position = position
@@ -97,6 +99,7 @@ class BackgroundLayer: SKNode {
         }
     }
     
+    //create comets particles
     func createComet() -> SKEmitterNode{
         
         let particle = SKEmitterNode(fileNamed: "Comets.sks")
@@ -114,9 +117,4 @@ class BackgroundLayer: SKNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    
-    
-    
 }
