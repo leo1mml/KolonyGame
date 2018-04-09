67ddac757fd60f630b70c101eb1e3bf89a751361
Isaias Melo <ifm394@gmail.com>
2018-04-09 14:56:49 -0300

    Initializing the HudLayer implementation

---
+++ KolonyGame Shared/GameScene/Layers/HudLayer/Entities/ScoreEntity.swift
@@ -0,0 +1,15 @@
+//
+//  ScoreEntity.swift
+//  KolonyGame iOS
+//
+//  Created by Isaias Fernandes on 09/04/2018.
+//  Copyright © 2018 Leonel Menezes. All rights reserved.
+//
+
+import GameplayKit
+import SpriteKit
+
+
+class ScoreEntity : GKEntity {
+    
+}
---
+++ KolonyGame Shared/GameScene/Layers/HudLayer/HudLayer.swift
@@ -0,0 +1,16 @@
+//
+//  HudLayer.swift
+//  KolonyGame iOS
+//
+//  Created by Isaias Fernandes on 09/04/2018.
+//  Copyright © 2018 Leonel Menezes. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class HudLayer: SKNode {
+    
+}
+
+
