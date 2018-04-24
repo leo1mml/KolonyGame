21a218f486fbbacbfc1373f314d8ac73de9a0bf1
leo1mml <mml_leo@hotmail.com>
2018-04-24 15:24:41 -0300

    GAME LAYER ALL COMMENTED

--- KolonyGame Shared/GameScene/Layers/GameLayer/GameLayer.swift
+++ KolonyGame Shared/GameScene/Layers/GameLayer/GameLayer.swift
@@ -9,12 +9,24 @@ import SpriteKit
 
 class GameLayer: SKNode {
     
+    // MARK: - VARIABLES
+    
+    ///Game layer size reference.
     var size: CGSize?
+    
+    ///Entity manager that manages all entities within the game layer.
     var entityManager : EntityManagerGameLayer?
+    
+    ///The black hole entity reference
     var blackHole : BlackHoleEntity?
+    
+    ///This variable is used to tell if the game layer is allowed in user interaction or not.
     var cantTouchThis: Bool = false
+    
+    /// Checks if the user has to give a tap before launching the rocket.
     var tapToLaunch = true
     
+    /// The rocket reference of the first element in rocket list. When its value is changed  it makes some configurations to itself.
     var rocketToLaunch : RocketEntity? {
         didSet {
             if let sprite = rocketToLaunch?.component(ofType: SpriteComponent.self)?.node{
@@ -22,15 +34,32 @@ class GameLayer: SKNode {
             }
         }
     }
+    
+    ///The rocket list in order of launch.
     var rocketList = [RocketEntity]()
+    
+    /// The variation of time between updates.
     var deltaTime: TimeInterval = 0
+    
+    /// The random interval which the black hole should invert its rotation.
     var actionInterval: TimeInterval = NumbersUtil.randomDouble(min: 10, max: 20)
+    
+    /// The reference to the red planet.
     var planetRed : PlanetEntity?
+    
+    /// The reference to the blue planet.
     var planetBlue : PlanetEntity?
+    
+    /// The reference to the green planet.
     var planetGreen : PlanetEntity?
+    
+    /// The reference to the yellow planet.
     var planetYellow : PlanetEntity?
     
+    /// The black hole sound.
     var blackHoleSound: SKAudioNode = SKAudioNode(fileNamed: "blackHole")
+    
+    // MARK: - INIT
     
     init(size: CGSize) {
         super.init()
@@ -38,6 +67,7 @@ class GameLayer: SKNode {
         entityManager = EntityManagerGameLayer(gameLayer: self)
     }
     
+    // MARK: - SOUND
     func playSound() {
         self.addChild(blackHoleSound)
     }
@@ -46,11 +76,15 @@ class GameLayer: SKNode {
         self.blackHoleSound.removeFromParent()
     }
     
+    
+    // MARK: - CONFIGURATION
     func configureLayer() {
         createBlackHole()
         createRocketList()
     }
     
+    
+    ///Creates a black hole in the screen, sets it to its specific position and adds its children, the planets.
     func createBlackHole() {
         let size = CGSize(width: (self.size?.height)! * 0.31, height: (self.size?.height)! * 0.31)
         self.blackHole = BlackHoleEntity(size: size)
@@ -66,8 +100,12 @@ class GameLayer: SKNode {
         createPlanets()
         self.blackHole?.moveOtherWay()
     }
-    
-    //Get the black hole position
+    /**
+     Get the black hole position
+     - Parameters:
+        - layer: the layer which the position should be calculated
+     - Returns: The position of the sprite relative to the layer passed as argument, or de point zero to the current layer.
+     */
     func blackHolePosition(layer: SKNode) -> CGPoint {
         if let spriteComponent = blackHole?.component(ofType: SpriteComponent.self) {
             let position = spriteComponent.node.position
@@ -76,6 +114,9 @@ class GameLayer: SKNode {
         return CGPoint.zero
     }
 
+    /**
+     Create all the planets around the black hole
+     */
     func createPlanets() {
         if let blackHoleSprite = self.blackHole?.component(ofType: SpriteComponent.self){
             self.planetBlue = createPlanet(size: CGSize(width: (self.size?.height)! * 0.11, height: (self.size?.height)! * 0.11), properties: PlanetProperties.blue, position: CGPoint(x: 0, y: -(blackHoleSprite.node.size.height/2)), zPosition: 20, rotationAngle: -Double.pi * 2, duration: 3)
@@ -85,6 +126,18 @@ class GameLayer: SKNode {
         }
     }
     
+    
+    /**
+     Creates a planet on the screen
+     - Parameters:
+        - size: The planet's size
+        - properties: The enum with the properties of the planet, default values: blue, yellow, green, red.
+        - position: The planet position relative to its layer
+        - zPosition: The zPosition of the planet, it's used to avoid overlaying of other items.
+        - rotationAngle: The angle in radians that the planet should spin, the rotation is usually used to cancel the default rotation of the black hole.
+        - duration: The duration that the planet should perform the rotation.
+     - Returns: The planet entity to be added to the entity manager
+     */
     func createPlanet(size: CGSize, properties: PlanetProperties, position: CGPoint, zPosition: CGFloat, rotationAngle: Double, duration: Double) -> PlanetEntity{
         let planet = PlanetEntity(property: properties, size: size)
         if let planetSprite = planet.component(ofType: SpriteComponent.self) {
@@ -96,6 +149,10 @@ class GameLayer: SKNode {
         return planet
     }
 
+    
+    /**
+     Create the rockets, place them in the right positions, and add them to the layer and entity manager.
+     */
     func createRocketList() {
         var positionX = (self.size?.width)! / 2
         for index in 0...2 {
@@ -122,6 +179,13 @@ class GameLayer: SKNode {
         }
     }
     
+    // MARK: - UTILS
+    
+    /**
+     Resize the planet to a bigger size
+     - Parameters:
+        - rocket: The rocket entity which contains the sprite to be resized.
+     */
     func resizeRocketToBig(rocket: RocketEntity) {
         if let sprite = rocket.component(ofType: SpriteComponent.self)?.node {
             let resizeAction = SKAction.scale(to: 1, duration: 0.5)
@@ -131,6 +195,11 @@ class GameLayer: SKNode {
         }
     }
     
+    /**
+     Resize the planet to a smaller size
+     - Parameters:
+     - rocket: The rocket entity which contains the sprite to be resized.
+     */
     func resizeRocketToNormal(rocket: RocketEntity) {
         if let sprite = rocket.component(ofType: SpriteComponent.self)?.node {
             let resizeAction = SKAction.scale(to: 0.75, duration: 0.5)
@@ -138,6 +207,11 @@ class GameLayer: SKNode {
         }
     }
     
+    /**
+     This method is used to recicle a rocket entity after its collision. After the recicle the entity will now be configured with new random properties.
+     - Parameters:
+        - rocket: The rocket entity to be recicled.
+     */
     func recicleShip(rocket: RocketEntity) {
         rocket.stop()
         let properties = RocketType.generateRandomShipProperties()
@@ -164,6 +238,7 @@ class GameLayer: SKNode {
         self.rocketList.append(rocket)
     }
     
+    ///After a rocket is launched this method can move the list and prepare the next ship in line to be Idle, but it can't be launched yet.
     func moveRocketList() {
         for index in 0...rocketList.count - 1 {
             if let sprite = rocketList[index].component(ofType: SpriteComponent.self)?.node {
@@ -180,6 +255,7 @@ class GameLayer: SKNode {
         }
     }
     
+    ///Launch the rocket at the beginning of the queue and removes it from the rocket list.
     func lauchRocket() {
         if(rocketList.count > 0){
             self.rocketToLaunch = nil
@@ -192,6 +268,16 @@ class GameLayer: SKNode {
         }
     }
     
+    
+    /**
+     Makes the rocket disappear in a spiral movement until it gets to a specific point.
+     - Parameters:
+        - centerPoint: The point which the black hole should go with the spiral movement.
+        - startRadius: The distance between the center point and the rocket to be flushed.
+        - endRadius: The final distance between the center point and the rocket at the end of the movement.
+        - angle: The angle which the movement should go through.
+        - duration: The duration of the movement.
+     */
     func flushRocketTo(centerPoint: CGPoint, startRadius: CGFloat, endRadius: CGFloat, angle: CGFloat, duration: TimeInterval){
         for rocket in rocketList {
             rocket.spriteComponent?.node.physicsBody?.categoryBitMask = PhysicsCategory.None
@@ -208,6 +294,13 @@ class GameLayer: SKNode {
         }
     }
     
+    
+    // MARK: - REPPLAY
+    
+    /**
+     Starts the game over animations
+     - Parameter finished: A callback that should be performed once the animations are over.
+     */
     func startGameOverEffect(finished: (() -> Void)?) {
         DispatchQueue.main.async {
             
@@ -231,6 +324,9 @@ class GameLayer: SKNode {
         
     }
     
+    /**
+     Reconfigures the game layer elements to its specific positions.
+     */
     func resetupGameLayer () {
         
         self.stopSound()
@@ -242,6 +338,17 @@ class GameLayer: SKNode {
         }
     }
 
+    
+    /**
+     Moves a node to the black hole position
+     - Parameters:
+        - node: The node that should perform the move action
+        - duration: The duration of the movement.
+        - durantionDecreaseAlpha: The duration of the decrease size action of the node.
+        - nextPosition: The position that the element should go after the game over effect.
+        - nextScale: The scale size which the element should be after it concludes the movement.
+        - finished: The callback that should be performed once the animation is over.
+     */
     private func moveToBlackHoleposition (node: SKSpriteNode, duration: TimeInterval, durantionDecreaseAlpha: TimeInterval, nextPosition: CGPoint, nextScale: Float, finished: (() -> Void)?) {
 
         //move to black hole position, set scale 0 and remove of screen
@@ -258,17 +365,29 @@ class GameLayer: SKNode {
         }
     }
     
+    /**
+     Moves the sprite node to a position
+     - Parameters:
+        - node: The node that should move
+        - nextPosition: The position of the node when the movement ends.
+     */
     func reconfigureSprite (_ node: SKSpriteNode, _ nextPosition: CGPoint) {
         node.run(SKAction.move(to: nextPosition, duration: TimeInterval(0.1)))
     
     }
 
+    
+    /**
+     Gets the scene reference, the scene which this layer is at.
+     */
     private func sceceReference () ->  GameScene! {
         if let parent = self.parent as? GameScene {
             return parent
         }
         return nil
     }
+    
+    // MARK: - TOUCH
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             
@@ -302,6 +421,11 @@ class GameLayer: SKNode {
         
     }
     
+    // MARK: - UPDATE
+    /**
+     The update abstraction that should be called within the gameScene.
+     - Parameter deltaTime: the time interval between one update and the previous one.
+     */
     func update(deltaTime: TimeInterval) {
         if deltaTime < 12000{
             self.deltaTime = self.deltaTime + deltaTime
